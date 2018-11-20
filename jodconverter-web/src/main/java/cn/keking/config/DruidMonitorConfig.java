a74ba5009498f28e7f41edeca96772ae8bb63259
chenyanting <chenyanting666@sina.com>
2018-11-20 21:26:46 +0800

    监听去除黑白名单

--- jodconverter-web/src/main/java/cn/keking/config/DruidMonitorConfig.java
+++ jodconverter-web/src/main/java/cn/keking/config/DruidMonitorConfig.java
@@ -23,10 +23,6 @@ public class DruidMonitorConfig {
         logger.info("init Druid Monitor Servlet ...");
         ServletRegistrationBean servletRegistrationBean = new ServletRegistrationBean(new StatViewServlet(),
                 "/druid/*");
-        // IP白名单
-        servletRegistrationBean.addInitParameter("allow", "10.20.46.87,10.20.46.106,10.20.46.126,127.0.0.1");
-        // IP黑名单(共同存在时，deny优先于allow)
-        servletRegistrationBean.addInitParameter("deny", "192.168.1.100");
         // 控制台管理用户
         servletRegistrationBean.addInitParameter("loginUsername", "admin");
         servletRegistrationBean.addInitParameter("loginPassword", "eim");
